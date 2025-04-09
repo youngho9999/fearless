0c63dc20c9378513c756ac44a541e164ed0fdfb5
youngho9999 <aplblypt@gmail.com>
2025-04-09 17:25:59 +0900

    [fix] 웹소켓 연결 엔드포인트 변경

--- src/main/java/live/feardraft/global/config/WebSocketConfig.java
+++ src/main/java/live/feardraft/global/config/WebSocketConfig.java
@@ -24,7 +24,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/connect")
+        registry.addEndpoint("/api/connect")
                 .setAllowedOrigins(origin);
     }
 }
